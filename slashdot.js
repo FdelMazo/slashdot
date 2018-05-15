7f3c08c97741609ad84151f22cca4bb408e72d09
Federico del Mazo <federicodelmazo@hotmail.com>
2018-05-15 13:31:26 -0300

    Cambio de regexp a sumatoria de input

--- slashdot.js
+++ slashdot.js
@@ -1,26 +1,52 @@
-var repeticion = 1;
-var incrementar = false;
+var AVAILABLECHARS = new RegExp("^[\\.\>]*$")
+
+var sumatoria = 0;
+var sumatoriaAnt = 0;
+var nivel = 0;
 
 $(document).ready(function(){     
-    function testTextArea(){
+    
+    function jugar(){    
         var input = $("#slashdot").val();
-        var regex = getRegexp();
-        if (regex.test(input)) {
-            incrementar = true;
-            $("#txtHere").append("Punto!<br>");
-            $("#slashdot").val('');
+        if (!AVAILABLECHARS.test(input)){
+            perder();
+        }    
+        var inputPicos = input.replace(/\./g,'')
+        var inputSumatoria = inputPicos.length;
+        if (inputSumatoria == sumatoria) {
+            incrementar()
         }
+        else if (inputSumatoria == sumatoriaAnt) return;   
+        else  {
+            perder()
+        }       
     }
 
-    function getRegexp(){
-        if (incrementar){
-            repeticion +=1;
-            incrementar = false;
-        }
-        var puntos = new RegExp("(\\.)+", "g");
-        var picos = new RegExp("(\>){" + repeticion + "}", "g");
-        var REGEXP = new RegExp(puntos.source + picos.source + puntos.source, "g");
-        return REGEXP;
+    function perder(){
+        $("#resultados").text("Perdiste :(");
+        $("#slashdot").val('');
+        sumatoria = 0
+        nivel = 0
+        sumatoriaAnt = 0
     }
-    $("#slashdot").keydown(testTextArea);
+
+    function incrementar(){
+        if (sumatoria == 0){
+            sumatoria = 1
+        }
+        else {
+            $("#resultados").append("Punto!! - "+ nivel + "<br>");
+            sumatoriaAnt = sumatoria
+            sumatoria = sumatoria + (nivel+1)
+        }
+        nivel += 1;
+    }
+
+    var bool = true
+    $(document).bind("keydown", function (event) {
+        if (event.keyCode == 190 && !event.shiftKey) { //40 is the keyCode of down arrow key
+            jugar()
+        }
+    });
+    
 });
