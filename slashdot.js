7c879847d6fabbffa2926c797fe70b532aeab04b
Flox <florrr1997@gmail.com>
2018-05-17 18:04:56 -0300

    Agrego un contador para saber cuantos slashes vas

--- slashdot.js
+++ slashdot.js
@@ -4,33 +4,35 @@ var sumatoria = 0;
 var sumatoriaAnt = 0;
 var nivel = 0;
 
-$(document).ready(function(){     
+$(document).ready(function(){
     $("#slashdot").val('');
-    
-    function jugar(){   
-        var input = $("#slashdot").val();   
+
+    function jugar(){
+        var input = $("#slashdot").val();
         var inputPicos = input.replace(/\./g,'')
         var inputSumatoria = inputPicos.length;
         if (inputSumatoria == sumatoria) {
-            incrementar()
+            incrementar();
+            document.getElementById('count').innerHTML = nivel-1;
         }
-        else if (inputSumatoria == sumatoriaAnt) return;   
+        else if (inputSumatoria == sumatoriaAnt) return;
         else  {
-            perder()
-        }       
+            perder();
+        }
     }
 
     function perder(){
-        var recordAnt = $("#recordVal").text(); 
+        var recordAnt = $("#recordVal").text();
         var recordAct = nivel-1;
         if (recordAct > recordAnt){
             $("#recordVal").text(nivel -1);
-        }   
+        }
         sumatoria = 0
         nivel = 0
         sumatoriaAnt = 0
         $("#slashdot").val('');
-        $("#puntosVal").text(0);   
+        $("#puntosVal").text(0);
+        document.getElementById('count').innerHTML = 0;
 
     }
 
@@ -39,7 +41,7 @@ $(document).ready(function(){
             sumatoria = 1
         }
         else {
-            $("#puntosVal").text(nivel);   
+            $("#puntosVal").text(nivel);
             sumatoriaAnt = sumatoria
             sumatoria = sumatoria + (nivel+1)
         }
@@ -48,14 +50,14 @@ $(document).ready(function(){
 
     function latamToEng(){
         var input = $("#slashdot").val();
-        $("#slashdot").val(input.replace(/\:/g, ">")); 
+        $("#slashdot").val(input.replace(/\:/g, ">"));
     }
 
     function validarCaracteres(){
         var input = $("#slashdot").val();
         if (!AVAILABLECHARS.test(input)){
             perder();
-        } 
+        }
     }
 
     $(document).bind("keydown", function (event) {
@@ -70,9 +72,9 @@ $(document).ready(function(){
     });
 
     $(document).bind("keyup", function (event) {
-        if (event.keyCode == 190 && !event.shiftKey) { 
+        if (event.keyCode == 190 && !event.shiftKey) {
             perder()
         }
     });
-    
+
 });
